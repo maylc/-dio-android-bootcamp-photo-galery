2e3289da22ba33a6af6d967ea00263c58635efd3
Mayara Felix <maylc.dev@gmail.com>
2021-09-20 23:43:16 -0400

    Implement logic to select image on the galery.

--- Application/app/src/main/java/io/github/maylcf/application/MainActivity.kt
+++ Application/app/src/main/java/io/github/maylcf/application/MainActivity.kt
@@ -1,11 +1,93 @@
 package io.github.maylcf.application
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.appcompat.app.AppCompatActivity
+import io.github.maylcf.application.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var resultLauncher : ActivityResultLauncher<Intent>
+
+    companion object {
+        const val requestStoragePermissionCode = 100
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        resultLauncher = registerForActivityResult(StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                updateImage(result.data?.data)
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        binding.selectImageButton.setOnClickListener {
+            if (shouldRequestPermission()) {
+                if (isMissingReadStoragePermission()) {
+                    requestReadStoragePermission()
+                } else {
+                    pickImageFromGalery()
+                }
+            } else {
+                pickImageFromGalery()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            requestStoragePermissionCode -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pickImageFromGalery()
+                } else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun shouldRequestPermission(): Boolean {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+    }
+
+    private fun isMissingReadStoragePermission(): Boolean {
+        return checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
+    }
+
+    private fun requestReadStoragePermission() {
+        val permission = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+        requestPermissions(permission, requestStoragePermissionCode)
+    }
+
+    private fun pickImageFromGalery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        resultLauncher.launch(intent)
+    }
+
+    private fun updateImage(uri: Uri?) {
+        binding.selectedImage.setImageURI(uri)
     }
 }
