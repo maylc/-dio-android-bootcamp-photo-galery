b7f78cce9ca2f232f47fb0c4abafde7194b018f1
Mayara Felix <maylc.dev@gmail.com>
2021-09-22 16:29:18 -0400

    launch camera intent.

--- Application/app/src/main/java/io/github/maylcf/application/MainActivity.kt
+++ Application/app/src/main/java/io/github/maylcf/application/MainActivity.kt
@@ -2,11 +2,13 @@ package io.github.maylcf.application
 
 import android.Manifest
 import android.app.Activity
+import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
@@ -17,9 +19,13 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var cameraResultLauncher: ActivityResultLauncher<Intent>
+
+    private var imageUri: Uri? = null
 
     companion object {
         const val requestStoragePermissionCode = 100
+        const val requestCameraPermissionCode = 101
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +37,12 @@ class MainActivity : AppCompatActivity() {
         resultLauncher = registerForActivityResult(StartActivityForResult()) { result ->
             if (result.resultCode == Activity.RESULT_OK) {
                 updateImage(result.data?.data)
+            }
+        }
+
+        cameraResultLauncher = registerForActivityResult(StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                updateImage(imageUri)
             }
         }
     }
@@ -49,6 +61,31 @@ class MainActivity : AppCompatActivity() {
                 pickImageFromGalery()
             }
         }
+
+        binding.openCameraButton.setOnClickListener {
+            if (shouldRequestPermission()) {
+                if (isMissingCameraPermission() || isMissingWriteStoragePermission()) {
+                    requestWriteAndCameraPermissions()
+                } else {
+                    launchCamera()
+                }
+            } else {
+                launchCamera()
+            }
+        }
+    }
+
+    private fun launchCamera() {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "new_photo")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "new photo - bootcamp")
+
+        imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+
+        cameraResultLauncher.launch(cameraIntent)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -56,6 +93,15 @@ class MainActivity : AppCompatActivity() {
             requestStoragePermissionCode -> {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     pickImageFromGalery()
+                } else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+            requestCameraPermissionCode -> {
+                if (grantResults.size > 1
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    launchCamera()
                 } else {
                     Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
                 }
@@ -73,9 +119,22 @@ class MainActivity : AppCompatActivity() {
         return checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
     }
 
+    private fun isMissingCameraPermission(): Boolean {
+        return checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED
+    }
+
+    private fun isMissingWriteStoragePermission(): Boolean {
+        return checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
+    }
+
     private fun requestReadStoragePermission() {
         val permission = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
         requestPermissions(permission, requestStoragePermissionCode)
+    }
+
+    private fun requestWriteAndCameraPermissions() {
+        val permissions = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        requestPermissions(permissions, requestCameraPermissionCode)
     }
 
     private fun pickImageFromGalery() {
